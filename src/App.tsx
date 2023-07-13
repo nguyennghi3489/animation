f052fe7fa98ca0c108cb712d6a47259e04c89e13
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-07-13 15:31:13 +0700

    feat: disable swipe

--- src/App.tsx
+++ src/App.tsx
@@ -284,6 +284,7 @@ function App() {
           showFullscreenButton={false}
           showPlayButton={false}
           onSlide={hanldeSlide}
+          disableSwipe
         />
       </div>
       <div
